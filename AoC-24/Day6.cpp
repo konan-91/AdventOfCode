04fb2dafadca9862bce065d750e476a7780275eb
konan-91 <konan.g591@gmail.com>
2024-12-26 02:23:03 +0000

    Day 6 1/2

--- AoC-24/Day6.cpp
+++ AoC-24/Day6.cpp
@@ -1,6 +1,4 @@
-//
 // Advent Of Code 2024++, Day 6.
-//
 
 #include <fstream>
 #include <iostream>
@@ -30,15 +28,94 @@ std::vector<std::vector<char>> readFile(const std::string& path) {
     return lab;
 }
 
+bool loopCheck(const auto& target, auto currentPosition, auto& loopCheckSet, const auto& lab, const int& yMax, const int& xMax) {
+    while (currentPosition.first[0] > 0 && currentPosition.first[0] < yMax && currentPosition.first[1] > 0 && currentPosition.first[1] < xMax) {
+
+        if (currentPosition == target) {
+            return true;
+        }
+
+        /*
+        if (loopCheckSet.contains(currentPosition)) {
+            return false;
+        }
+        loopCheckSet.insert(currentPosition);
+        */
+
+        if (currentPosition.second == "north") {
+            if (lab[currentPosition.first[0] - 1][currentPosition.first[1]] == '#') {
+                currentPosition.second = "east";
+            } else {
+                --currentPosition.first[0];
+            }
+        }
+
+        else if (currentPosition.second == "east") {
+            if (lab[currentPosition.first[0]][currentPosition.first[1] + 1] == '#') {
+                currentPosition.second = "south";
+            } else {
+                ++currentPosition.first[1];
+            }
+        }
+
+        else if (currentPosition.second == "south") {
+            if (lab[currentPosition.first[0] + 1][currentPosition.first[1]] == '#') {
+                currentPosition.second = "west";
+            } else {
+                ++currentPosition.first[0];
+            }
+        }
+
+        else if (currentPosition.second == "west") {
+            if (lab[currentPosition.first[0]][currentPosition.first[1] - 1] == '#') {
+                currentPosition.second = "north";
+            } else {
+                --currentPosition.first[1];
+            }
+        }
+    }
+
+    return false;
+}
+
 int simulatePatrol(const std::string& path) {
     const std::vector<std::vector<char> > lab = readFile(path);
+/*
+    const std::vector<std::vector<char> > lab = {
+        {'.','#','.','.','.','.'},
+        {'.','0','.','0','.','#'},
+        {'#','.','.','#','.','.'},
+        {'.','.','#','.','.','.'},
+        {'0','^','.','.','.','#'},
+        {'.','.','.','.','#','.'},
+    };
+
+    const std::vector<std::vector<char> > lab = {
+        {'.','.','.','.','#','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','.','.','#'},
+        {'.','.','.','.','.','.','.','.','.','.'},
+        {'.','.','#','.','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','.','#','.','.'},
+        {'.','.','.','.','.','.','.','.','.','.'},
+        {'.','#','.','0','^','.','.','.','.','.'},
+        {'.','.','.','.','.','.','X','X','#','.'},
+        {'#','X','.','X','.','.','.','.','.','.'},
+        {'.','.','.','.','.','.','#','X','.','.'}
+    };
+    */
+    int step = 0;
+    // DON'T CALL THE FUCKING FUNCTION IF WHERE YOU WOULD BE PLACING THE # IS A ^. LOGIC NEEDS TO GO IN simulatePatrol() !!!!!!!!!!!!!!!!!!
+    std::set<std::pair<std::array<int, 2>, std::string>> loopCheckSet;
     std::set<std::array<int, 2>> positionSet;
     std::array currentPosition = {0, 0};
     std::string direction = "north";
+    int loopCount = 0;
 
     for (int i = 0; i < lab.size(); i++) {
         for (int j = 0; j < lab[0].size(); j++) {
             if (lab[i][j] == '^') {
+                currentPosition = {i, j};
+            }
         }
     }
 
@@ -48,34 +125,69 @@ int simulatePatrol(const std::string& path) {
     // Simulate traversal, counting unique positions visited
     while (currentPosition[0] > 0 && currentPosition[0] < yMax && currentPosition[1] > 0 && currentPosition[1] < xMax) {
         positionSet.insert(currentPosition);
+        loopCheckSet.insert({currentPosition, direction});
 
         if (direction == "north") {
             if (lab[currentPosition[0] - 1][currentPosition[1]] == '#') {
                 direction = "east";
             } else {
+                if (lab[currentPosition[0] - 1][currentPosition[1]] != '^') {
+                    if (loopCheck(std::make_pair(currentPosition, direction), std::make_pair(currentPosition, "east"), loopCheckSet, lab, yMax, xMax)) {
+                        loopCount++;
+                    }
+                }
+
                 currentPosition[0]--;
             }
-        } else if (direction == "east") {
+        }
+
+        else if (direction == "east") {
             if (lab[currentPosition[0]][currentPosition[1] + 1] == '#') {
                 direction = "south";
             } else {
+                if (lab[currentPosition[0]][currentPosition[1] + 1] != '^') {
+                    if (loopCheck(std::make_pair(currentPosition, direction), std::make_pair(currentPosition, "south"), loopCheckSet, lab, yMax, xMax)) {
+                        loopCount++;
+                    }
+                }
+
                 currentPosition[1]++;
             }
-        } else if (direction == "south") {
+        }
+
+        else if (direction == "south") {
             if (lab[currentPosition[0] + 1][currentPosition[1]] == '#') {
                 direction = "west";
             } else {
+                if (lab[currentPosition[0] + 1][currentPosition[1]] != '^') {
+                    if (loopCheck(std::make_pair(currentPosition, direction), std::make_pair(currentPosition, "west"), loopCheckSet, lab, yMax, xMax)) {
+                        loopCount++;
+                    }
+                }
+
                 currentPosition[0]++;
             }
-        } else if (direction == "west") {
+       }
+
+        else if (direction == "west") {
             if (lab[currentPosition[0]][currentPosition[1] - 1] == '#') {
                 direction = "north";
             } else {
+                if (lab[currentPosition[0]][currentPosition[1] - 1] != '^') {
+                    if (loopCheck(std::make_pair(currentPosition, direction), std::make_pair(currentPosition, "north"), loopCheckSet, lab, yMax, xMax)) {
+                        loopCount++;
+                    }
+                }
+
                 currentPosition[1]--;
             }
         }
+
+        step++;
+        std::cout << step << "\n";
     }
 
+    std::cout << loopCount << std::endl;
     return static_cast<int>(positionSet.size()) + 1;
 }
 
