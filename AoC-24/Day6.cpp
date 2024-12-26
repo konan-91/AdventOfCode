34f8b1b34a0df12dee31db630936dda5145bb1fa
konan-91 <konan.g591@gmail.com>
2024-12-26 18:57:50 +0000

    Day 6 v2

--- AoC-24/Day6.cpp
+++ AoC-24/Day6.cpp
@@ -62,57 +62,109 @@ Lab readFile(const std::string& path) {
 }
 
 // Change the actual lab value as it's faster than creating a copy - just be sure to change it back when you're finished.
-int countLoops(Lab& lab, std::set<std::array<int, 3>>& uniquePos) {
+int countLoops(Lab& lab, const std::set<std::array<int, 3>>& obstaclePos) {
     int loopCount = 0;
+    int iter = 0;
 
-    // REMOVE lab.startPos from uniquePos before starting, or else you may create a # on the starting position!
-    // uniquePos.erase(lab.startPos);
+    for (std::array<int, 3> pos : obstaclePos) {
+        std::set<std::array<int, 3>> loopCheck;
+        auto posBackup = pos;
 
-    // Check every uniquePos for loops (start positions where
+        // Ensure the position '#' will be entered in is not the start position??
+        if (lab.grid[pos[0]][pos[1]] == '^') {
+            continue;
+        }
 
+        // Modify grid for simulation
+        lab.grid[pos[0]][pos[1]] = '#';
 
+        // Start simulation
+        pos = {lab.startPos[0], lab.startPos[0], UP};
+        while (pos[0] > 0 && pos[0] < lab.yMax && pos[1] > 0 && pos[1] < lab.xMax) {
+            if (loopCheck.contains(pos)) {
+                loopCount++;
+                std::cout << "Loop found! loopCount: " << loopCount << "\n";
+                break;
+            }
+            loopCheck.insert(pos);
+
+            if (pos[2] == UP) {
+                if (lab.grid[pos[0] - 1][pos[1]] == '#') {
+                    pos[2]  = RIGHT;
+                } else {
+                    pos[0]--;
+                }
+            } else if (pos[2]  == RIGHT) {
+                if (lab.grid[pos[0]][pos[1] + 1] == '#') {
+                    pos[2]  = DOWN;
+                } else {
+                    pos[1]++;
+                }
+            } else if (pos[2]  == DOWN) {
+                if (lab.grid[pos[0] + 1][pos[1]] == '#') {
+                    pos[2]  = LEFT;
+                } else {
+                    pos[0]++;
+                }
+            } else if (pos[2]  == LEFT) {
+                if (lab.grid[pos[0]][pos[1] - 1] == '#') {
+                    pos[2]  = UP;
+                } else {
+                    pos[1]--;
+                }
+            }
+        }
+
+        // Return grid back to normal!!
+        iter++;
+        std::cout << "Iterations so far: " << iter++ << "\n";
+        lab.grid[posBackup[0]][posBackup[1]] = '.';
+    }
 
     return loopCount;
 }
 
 std::pair<int, int> simulatePatrol(Lab& lab) {
-    std::set<std::array<int, 3>> uniquePosDir; // 3rs val is so we can tell what the orientation was at each position!
+    std::set<std::array<int, 3>> obstaclePos; // 3rs val is so we can tell what the orientation was at each position!
     std::set<std::array<int, 2>> uniquePos;
     std::array<int, 2> currentPos = lab.startPos;
     Direction dir = UP;
 
     while (currentPos[0] > 0 && currentPos[0] < lab.yMax && currentPos[1] > 0 && currentPos[1] < lab.xMax) {
         uniquePos.insert({currentPos[0], currentPos[1]});
-        uniquePosDir.insert({currentPos[0], currentPos[1], dir});
 
         if (dir == UP) {
            if (lab.grid[currentPos[0] - 1][currentPos[1]] == '#') {
                dir = RIGHT;
            } else {
+               obstaclePos.insert({currentPos[0] - 1, currentPos[1], UP});
                currentPos[0]--;
            }
         } else if (dir == RIGHT) {
             if (lab.grid[currentPos[0]][currentPos[1] + 1] == '#') {
                 dir = DOWN;
             } else {
+                obstaclePos.insert({currentPos[0], currentPos[1] + 1, RIGHT});
                 currentPos[1]++;
             }
         } else if (dir == DOWN) {
             if (lab.grid[currentPos[0] + 1][currentPos[1]] == '#') {
                 dir = LEFT;
             } else {
+                obstaclePos.insert({currentPos[0] + 1, currentPos[1], DOWN});
                 currentPos[0]++;
             }
         } else if (dir == LEFT) {
             if (lab.grid[currentPos[0]][currentPos[1] - 1] == '#') {
                 dir = UP;
             } else {
+                obstaclePos.insert({currentPos[0], currentPos[1] - 1, LEFT});
                 currentPos[1]--;
             }
         }
     }
 
-    return std::make_pair(uniquePos.size() + 1, countLoops(lab, uniquePos));
+    return std::make_pair(uniquePos.size() + 1, countLoops(lab, obstaclePos));
 }
 
 int main() {
