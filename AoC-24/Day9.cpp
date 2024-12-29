738bcb9c09baaed6023f9bb988722fa54a1a86af
konan-91 <konan.g591@gmail.com>
2024-12-29 23:52:22 +0000

    Day 9 init

--- AoC-24/Day9.cpp
+++ AoC-24/Day9.cpp
@@ -3,5 +3,41 @@
 
 // TODO: everything! Work in progress
 
+#include <fstream>
+#include <iostream>
+
+std::vector<char> readFile(const std::string& path) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file!" << std::endl;
+        return {};
+    }
+
+    std::string str;
+    std::getline(file, str);
+    std::vector<char> input(str.begin(), str.end());
+
+    return input;
+}
+
+expandDisk() {
+
+    return ;
+}
+
+compactDisk() {
+
+    return ;
+}
+
+size_t checkSum() {
+    size_t checksum ;
+
+    return checksum;
+}
+
 int main() {
+    // Move file blocks one at a time from the end of the disk to the leftmost free space block
+    std::vector<char> input = readFile("AoC-24/input_files/day_9/input.txt");
+    std::cout << input[0] << "\n";
 }
