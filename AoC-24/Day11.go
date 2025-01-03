801831af5031fa30a505997743ec82556ec14d8b
konan-91 <konan.g591@gmail.com>
2025-01-03 21:36:08 +0000

    Day 11: 1/2 Improved

--- AoC-24/Day11.go
+++ AoC-24/Day11.go
@@ -28,59 +28,53 @@ func readFile(path string) []uint64 {
 		intArr[i] = num
 	}
 
-	// intArr = []uint64{125, 17}
-
 	return intArr
 }
 
-func blink(stones []uint64) []uint64 {
-	changedStones := make([]uint64, 0, len(stones)*2)
-	for _, stone := range stones {
+// PT.2 SOLUTION -> Cache solutions in map. For each stone, before doing anything, check if the answer is in the cache.
+// If yes, use the answer and skip.
+// If no, calculate the answer and add to the cache.
+func blink(stones *[]uint64) []uint64 {
+	changedStones := make([]uint64, 0, len(*stones)*2)
+
+	for _, stone := range *stones {
 		if stone == 0 {
 			changedStones = append(changedStones, 1)
 			continue
 		}
 
-		// Convert to runes, verify whether it can be halved, verify whether RHS consists of 0's
-		runeStone := []rune(strconv.FormatUint(stone, 10))
+		runeStone := strconv.FormatUint(stone, 10)
 		N := len(runeStone)
+
 		if N%2 == 0 {
-			// Split in half
-			lhs := runeStone[0 : N/2]
-			rhs := runeStone[N/2 : N]
-
-			// No need to remove trailing 0's? Probably handled by ParseUint
-
-			// Convert back & append
-			lhsNum, err := strconv.ParseUint(string(lhs), 10, 64)
-			if err != nil {
-				panic(err)
-			}
-			rhsNum, err := strconv.ParseUint(string(rhs), 10, 64)
-			if err != nil {
-				panic(err)
-			}
-
+			// Split in half & append
+			lhsNum, _ := strconv.ParseUint(runeStone[0:N/2], 10, 64)
+			rhsNum, _ := strconv.ParseUint(runeStone[N/2:N], 10, 64)
 			changedStones = append(changedStones, lhsNum, rhsNum)
 		} else {
 			changedStones = append(changedStones, stone*2024)
 		}
 	}
 
-	//fmt.Println(changedStones)
 	return changedStones
 }
 
 func countStones(path string) int {
+	blinkNum := 0
 	stones := readFile(path)
-	for i := 0; i < 25; i++ {
-		fmt.Println("Current length:", len(stones))
-		stones = blink(stones)
+	for i := 0; i < 40; i++ {
+		if blinkNum == 25 {
+			fmt.Println("Stones at 25th blink:", len(stones))
+		} else {
+			fmt.Println("Blink no.:", blinkNum+1)
+		}
+		blinkNum++
+		stones = blink(&stones)
 	}
 	return len(stones)
 }
 
 func main() {
 	ans := countStones("input_files/day_11/input.txt")
-	fmt.Printf("Answer! -> %d\n", ans) // 194,433 -> too low! 194,557 LOL - useless code broke answer
+	fmt.Println("Stones at 75th blink:", ans) // 194,557 LOL - useless code broke answer
 }
