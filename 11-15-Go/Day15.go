ea6ee250888430eb4e963a5a4114f00b78402425
konan-91 <konan.g591@gmail.com>
2025-01-27 01:04:13 +0000

    Day 15 Init

---
+++ 11-15-Go/Day15.go
@@ -0,0 +1,23 @@
+// Advent Of Code 2024, Day 15.
+// https://adventofcode.com/2024/day/15
+
+package main
+
+import "fmt"
+
+func getGrid() {
+
+	return
+}
+
+func getInstructions() {
+	return
+}
+
+func simulateMovements() {
+	return
+}
+
+func main() {
+	fmt.Println("Check back later!")
+}
